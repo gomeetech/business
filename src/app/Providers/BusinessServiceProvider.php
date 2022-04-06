9b1c2f734c8ef3857cf4eb3c1f9606e572eb36f8
Doan LN <developer.vcc@gmail.com>
2022-04-07 01:33:44 +0700

    12

--- src/app/Providers/BusinessServiceProvider.php
+++ src/app/Providers/BusinessServiceProvider.php
@@ -25,9 +25,11 @@ class BusinessServiceProvider extends ServiceProvider
     {
         $this->loadViewsFrom(__DIR__.'/../../resources/views', 'busibess');
 
+        // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 
         if ($this->app->runningInConsole()) {
-            $this->registerMigrations();
+            // $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+
             $this->publishes([
                 __DIR__.'/../../database/migrations' => database_path('migrations'),
             ], 'busibess-migrations');
@@ -35,6 +37,7 @@ class BusinessServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../../resources/views' => base_path('resources/views/vendor/business'),
             ], 'busibess-views');
+            
 
             // $this->publishes([
             //     __DIR__.'/../config/passport.php' => config_path('passport.php'),
